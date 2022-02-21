42fbd32766afce100c2349e1cf90f62621314b70
Faisal Mahmud Rifat <mahmudrifat200@gmail.com>
2022-02-22 01:34:32 +0600

    added: AppInput , AppButton and SignInScreen

--- src/screens/SignInScreen.js
+++ src/screens/SignInScreen.js
@@ -1,11 +1,80 @@
-import { View, Text } from 'react-native';
-import React from 'react';
+import { useNavigation } from '@react-navigation/native';
+import React, { useState } from 'react';
+import { SafeAreaView, Text, View } from 'react-native';
+import FormButton from '../components/shared/AppButton';
+import FormInput from '../components/shared/AppInput';
+import { COLORS } from '../constants/theme';
 
 const SignInScreen = () => {
+   const navigation = useNavigation();
+   const [email, setEmail] = useState('');
+   const [password, setPassword] = useState('');
+
+   const handleOnSubmit = () => {
+      if (email !== '' && password !== '') {
+         //Sign In
+      }
+   };
    return (
-      <View>
-         <Text>SignInScreen</Text>
-      </View>
+      <SafeAreaView
+         style={{
+            backgroundColor: COLORS.white,
+            flex: 1,
+            alignItems: 'center',
+            justifyContent: 'flex-start',
+            padding: 20,
+         }}
+      >
+         {/* Header */}
+         <Text
+            style={{
+               fontSize: 25,
+               color: COLORS.black,
+               fontWeight: 'bold',
+               marginVertical: 32,
+            }}
+         >
+            Sign In
+         </Text>
+         {/* Email */}
+         <FormInput
+            labelText="Email"
+            placeholderText="Enter Your Email"
+            onChangeText={value => setEmail(value)}
+            value={email}
+            keyboardType={'email-address'}
+         />
+         {/* Password */}
+         <FormInput
+            labelText="Password"
+            placeholderText="Enter Your Password"
+            onChangeText={value => setPassword(value)}
+            value={password}
+            keyboardType={'email-address'}
+         />
+         {/* Submit Button */}
+         <FormButton
+            labelText="Submit"
+            handleOnPress={handleOnSubmit}
+            style={{ width: '100%' }}
+         />
+         {/* footer */}
+         <View
+            style={{
+               flexDirection: 'row',
+               alignItems: 'center',
+               marginTop: 20,
+            }}
+         >
+            <Text>Don't have an account?</Text>
+            <Text
+               style={{ marginLeft: 4, color: COLORS.primary }}
+               onPress={() => navigation.navigate('SignUpScreen')}
+            >
+               Create Account
+            </Text>
+         </View>
+      </SafeAreaView>
    );
 };
 
